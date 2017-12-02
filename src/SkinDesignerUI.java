74dc0b0fb8f6ecffddf2adeacacf59680ea10294
lthompson <lthompson@infomedia.com.au>
2017-12-02 21:33:25 +1100

    Try timer based approach to handle multiple keys at once

--- src/SkinDesignerUI.java
+++ src/SkinDesignerUI.java
@@ -2,98 +2,110 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.HashSet;
+import java.util.Set;
 
 public class SkinDesignerUI extends JPanel implements KeyListener {
     boolean help = false;
-	boolean startDesigning = false;
+    boolean startDesigning = false;
     int x = 0;
     int y = 0;
     int K = 0;
     int L = 0;
     int J = 0;
 
+    private final Set<Integer> pressed = new HashSet<>();
+    {
+        javax.swing.Timer timer = new Timer(15, e -> {
+            if (pressed.size() > 0) {
+                for (int keyCode : pressed) {
+                    updatePositionsFromKeys(keyCode);
+                }
+                this.repaint();
+            }
+        });
+        timer.start();
+    }
+
     @Override
     public Dimension getPreferredSize() {
         return new Dimension(640, 480);
     }
 
-    public void keyTyped(KeyEvent e) {}
-    public void keyReleased(KeyEvent e) {}
 
-    public void keyPressed(KeyEvent e) {
-        if (e.getKeyCode() == KeyEvent.VK_C) {
+    private void updatePositionsFromKeys(int keyCode){
+        if (keyCode == KeyEvent.VK_C) {
             startDesigning = !startDesigning;
         }
-        if (e.getKeyCode() == KeyEvent.VK_D) {
-            x = x +2;
+        if (keyCode == KeyEvent.VK_D) {
+            x = x + 2;
         }
-        if (e.getKeyCode() == KeyEvent.VK_A) {
-            x = x -2;
+        if (keyCode == KeyEvent.VK_A) {
+            x = x - 2;
         }
-        if (e.getKeyCode() == KeyEvent.VK_S) {
-            y = y +2;
+        if (keyCode == KeyEvent.VK_S) {
+            y = y + 2;
         }
-        if (e.getKeyCode() == KeyEvent.VK_W) {
-            y = y -2;
+        if (keyCode == KeyEvent.VK_W) {
+            y = y - 2;
         }
-        if (e.getKeyCode() == KeyEvent.VK_L) {
+        if (keyCode == KeyEvent.VK_L) {
             L = L + 1;
             if (L > 255) {
-                L = L-1;
+                L = L - 1;
             }
         }
-        if (e.getKeyCode() == KeyEvent.VK_K) {
+        if (keyCode == KeyEvent.VK_K) {
             K = K + 1;
             if (K > 255) {
-                K = K-1;
+                K = K - 1;
             }
         }
-        if (e.getKeyCode() == KeyEvent.VK_J) {
+        if (keyCode == KeyEvent.VK_J) {
             J = J + 1;
             if (J > 255) {
-                J = J-1;
+                J = J - 1;
             }
         }
-        if(e.getKeyCode() == KeyEvent.VK_H){
-        	help = !help;
+        if (keyCode == KeyEvent.VK_H) {
+            help = !help;
         }
-        if(e.getKeyCode() == KeyEvent.VK_T){
-        	J = 0;
-        			K = 0;
-        			L = 0;
+        if (keyCode == KeyEvent.VK_T) {
+            J = 0;
+            K = 0;
+            L = 0;
         }
-        if(e.getKeyCode() == KeyEvent.VK_P){
-        	x = 0;
-        	y = 0;
+        if (keyCode == KeyEvent.VK_P) {
+            x = 0;
+            y = 0;
         }
-        if(e.getKeyCode() == KeyEvent.VK_1){
-        	x = x+10;
+        if (keyCode == KeyEvent.VK_1) {
+            x = x + 10;
         }
-        if (e.getKeyCode() == KeyEvent.VK_2){
-        	x = x+20;
+        if (keyCode == KeyEvent.VK_2) {
+            x = x + 20;
         }
-        if(e.getKeyCode() == KeyEvent.VK_3){
-        	x = x+30;
+        if (keyCode == KeyEvent.VK_3) {
+            x = x + 30;
         }
-        if(e.getKeyCode() == KeyEvent.VK_4){
-        	x = x+40;
+        if (keyCode == KeyEvent.VK_4) {
+            x = x + 40;
         }
-        if(e.getKeyCode() == KeyEvent.VK_5){
-        	x = x+50;
+        if (keyCode == KeyEvent.VK_5) {
+            x = x + 50;
         }
-        if(x > 610){
-        	x = 610;
+        if (x > 610) {
+            x = 610;
         }
-        if(x < 0){
-        	x = 0;
+        if (x < 0) {
+            x = 0;
         }
-        if(y < 0){
-        	y = 0;
+        if (y < 0) {
+            y = 0;
         }
-        if(y > 450){
-        	y = 450;
+        if (y > 450) {
+            y = 450;
         }
-        this.repaint();
     }
 
     @Override
@@ -124,14 +136,30 @@ public class SkinDesignerUI extends JPanel implements KeyListener {
         }
         graphics.setColor(Color.WHITE);
         graphics.drawString("H = help", 300, 240);
-if(help == true){
-	graphics.setColor(Color.WHITE);
-	graphics.fillRect(0,  0,  1000000,  1000000);
-graphics.setColor(Color.BLACK);
-graphics.drawString("C = start designing", 0, 20);
-graphics.drawString("p = reset location", 0, 50);
-}
-        
+        if (help == true) {
+            graphics.setColor(Color.WHITE);
+            graphics.fillRect(0, 0, 1000000, 1000000);
+            graphics.setColor(Color.BLACK);
+            graphics.drawString("C = start designing", 0, 20);
+            graphics.drawString("p = reset location", 0, 50);
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        pressed.add(e.getKeyCode());
+        updatePositionsFromKeys(e.getKeyCode());
+        this.repaint();
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        pressed.remove(e.getKeyCode());
     }
 }
 
